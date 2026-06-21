ec1e090bc1dde8cea35fb7da99065d0461e05ef2
abhisheksharma27198 <abhisheksharma.27198@gmail.com>
2026-06-21 18:09:16 +0530

    Create Orders.js

---
+++ Orders.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Orders() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>📦 Your Orders</h1>
+
+      <h3>No orders placed yet.</h3>
+
+      <p>Start shopping to see your orders here.</p>
+    </div>
+  );
+}
+
+export default Orders;
